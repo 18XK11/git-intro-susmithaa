9dc22e51282f59180bd062be34dbd4769ceddeff
susmithaa <susmithajaganathan99@gmail.com>
2021-02-22 11:40:45 +0530

    Hello world to Hello People

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -13,7 +13,7 @@ public class HelloWorldController {
     int i;
     @GetMapping
     public ResponseEntity<String> print(){
-        return new ResponseEntity<String>("Hello world", HttpStatus.OK);
+        return new ResponseEntity<String>("Hello People", HttpStatus.OK);
     }
 
     @GetMapping("{name}")
